1c75300b647bfbac305d6e6a11aaea85e2996db9
gregzap <65359420+gregzap@users.noreply.github.com>
2020-06-23 13:17:34 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,74 +1,99 @@
 window.onload = ustawKonona;
-var speedKonon=1;
+var speedKonon = 1;
 var licznikKonona = 0;
-var stopnie=1;
+var stopnie = 1;
 var takNie = 0;
+var modeHelp = 0;
 var kolor;
 var w = document.documentElement.clientWidth;
 window.addEventListener("resize", ustawKonona);
-function liczeSe(){
-    document.getElementById("licznik").innerHTML = licznikKonona;
+
+function liczeSe() {
+  document.getElementById("licznik").innerHTML = licznikKonona;
 }
-function zakrencKonona(){
-    var krenciol = document.getElementById("pic");
-     krenciol.style.transform = "rotate("+stopnie+"deg)"; 
-     if (takNie==0){
-         stopnie++;
-     }
-     else {
-         stopnie--;
-     }
+function zakrencKonona() {
+  var krenciol = document.getElementById("pic");
+  krenciol.style.transform = "rotate(" + stopnie + "deg)";
+  if (takNie == 0) {
+    stopnie++;
+  } else {
+    stopnie--;
+  }
 }
-function fajnyKolor(){
-     kolor = Math.floor(Math.random()*16777215).toString(16);
+function fajnyKolor() {
+  kolor = Math.floor(Math.random() * 16777215).toString(16);
 }
-function spidnijKonona(){
-    speedKonon++;
+function spidnijKonona() {
+  speedKonon++;
 }
-function zwolnijKonona(){
-    if (speedKonon>0){
-        speedKonon--;
-    }
+function zwolnijKonona() {
+  if (speedKonon > 0) {
+    speedKonon--;
+  }
 }
-function ustawKonona(){
-    w = document.documentElement.clientWidth;
-    var konon = document.getElementById("pic");
-    document.getElementById("siema").style.height = konon.height+4+ "px";
-    konon.style.left = w / 2 - konon.width / 2 +"px";
+function ustawKonona() {
+  w = document.documentElement.clientWidth;
+  var konon = document.getElementById("pic");
+  document.getElementById("siema").style.height = konon.height + 4 + "px";
+  konon.style.left = w / 2 - konon.width / 2 + "px";
+  zakrencKonona();
+}
+function ruszSie() {
+  var konon = document.getElementById("pic");
+  var start = w / 2 - konon.width / 2;
+  var id = setInterval(frame, 5);
+  function frame() {
+    liczeSe();
     zakrencKonona();
-}
-function ruszSie(){
-    var konon = document.getElementById("pic");
-    var start = w/2-konon.width/2;
-    var id = setInterval(frame, 5);
-    function frame(){
-        liczeSe();
-        zakrencKonona();
-        if (takNie == 0){
-            if (start>=w-konon.width-10){
-                //clearInterval(id)
-                //ustawKonona();
-                takNie=1;
-                fajnyKolor();
-                document.getElementById("siema").style.backgroundColor = "#"+kolor;
-                licznikKonona++;
-            } else {
-                start+=speedKonon;
-                konon.style.left = start + "px";
-            }
-        }
-        else {
-            if (start<=0){
-                //clearInterval(id)
-                //ustawKonona();
-                takNie=0;
-                fajnyKolor();
-                document.getElementById("siema").style.backgroundColor = "#"+kolor;
-                licznikKonona++;
-            } else {
-                start-=speedKonon;
-                konon.style.left = start + "px";
-            }
-        } 
+    //turnDark();
+    if (takNie == 0) {
+      if (start >= w - konon.width - 10) {
+        takNie = 1;
+        fajnyKolor();
+        document.getElementById("siema").style.backgroundColor = "#" + kolor;
+        licznikKonona++;
+      } else {
+        start += speedKonon;
+        konon.style.left = start + "px";
+      }
+    } else {
+      if (start <= 0) {
+        takNie = 0;
+        fajnyKolor();
+        document.getElementById("siema").style.backgroundColor = "#" + kolor;
+        licznikKonona++;
+      } else {
+        start -= speedKonon;
+        konon.style.left = start + "px";
+      }
     }
+  }
+}
+function turnDark() {
+  if (modeHelp == 0) {
+    document.getElementById("siema").style.backgroundColor = "#393e46";
+    document.getElementById("siema1").style.backgroundColor = "#393e46";
+    document.getElementById("siema2").style.backgroundColor = "#393e46";
+    document.getElementById("siema3").style.backgroundColor = "#393e46";
+    document.getElementById("siema4").style.backgroundColor = "#393e46";
+    document.getElementById("siema5").style.backgroundColor = "#393e46";
+    document.getElementById("siema6").style.backgroundColor = "#393e46";
+    document.getElementById("przycisk1").style.backgroundColor = "#393e46";
+    document.getElementById("przycisk2").style.backgroundColor = "#393e46";
+    document.body.style.backgroundColor = "#222831";
+    modeHelp = 1;
+  } else {
+    document.getElementById("siema").style.backgroundColor = "#3b6978";
+    document.getElementById("siema1").style.backgroundColor = "#3b6978";
+    document.getElementById("siema2").style.backgroundColor = "#3b6978";
+    document.getElementById("siema2").style.backgroundColor = "#3b6978";
+    document.getElementById("siema3").style.backgroundColor = "#3b6978";
+    document.getElementById("siema4").style.backgroundColor = "#3b6978";
+    document.getElementById("siema5").style.backgroundColor = "#3b6978";
+    document.getElementById("siema6").style.backgroundColor = "#3b6978";
+    document.getElementById("przycisk1").style.backgroundColor = "#3b6978";
+    document.getElementById("przycisk2").style.backgroundColor = "#3b6978";
+    document.body.style.backgroundColor = "#204051";
+    modeHelp = 0;
+  }
 }
